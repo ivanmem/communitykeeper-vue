c36d1e7fec66f70c61a9ed7a81316715c56066db
Ivan Melentyev <ivan.saxalin@mail.ru>
2023-10-06 19:00:02 +0300

    Группы: Отображение счётчиков в одну строку

--- src/pages/AGroups/AGroupCounters.vue
+++ src/pages/AGroups/AGroupCounters.vue
@@ -11,46 +11,34 @@ const groupRef = toRef(() => props.group);
 const counters = useGroupCounters(groupRef);
 </script>
 <template>
-  <VList
-    v-if="counters.length > 0"
-    class="a-group-counters"
-    density="compact"
-    mandatory
-  >
-    <VListItem
+  <div v-if="counters.length > 0" class="a-group-counters">
+    <VChip
       v-for="counter in counters"
       :key="counter.name"
-      width="100%"
+      :title="counter.name"
       @click="openLink(counter.link)"
     >
-      <template #prepend>
-        <div class="a-group-counters__icon">
-          <component :is="counter.icon" />
+      <div class="a-group-counters__counter">
+        <component :is="counter.icon" class="a-group-counters__icon" />
+        <div class="a-group-counter__count">
+          {{ counter.count }}
         </div>
-      </template>
-      <VSheet class="a-group-counters__counter">
-        <div>
-          {{ counter.name }}
-        </div>
-        <div class="a-group-counter__count" style="min-width: 36px">
-          <div class="a-group-counter__count-wrapper">
-            {{ counter.count }}
-          </div>
-        </div>
-      </VSheet>
-    </VListItem>
-  </VList>
+      </div>
+    </VChip>
+  </div>
 </template>
 <style lang="scss">
 .a-group-counters {
-  padding-top: 0;
+  display: flex;
+  gap: 6px;
+  padding: 8px var(--vkui--size_base_padding_horizontal--regular);
+  flex-wrap: wrap;
 }
 
 .a-group-counters__icon {
   display: flex;
   justify-content: center;
-  margin-right: 12px;
-  width: 48px;
+  pointer-events: none; // отключаем title у svg
 }
 
 .a-group-counters__counter {
@@ -60,6 +48,7 @@ const counters = useGroupCounters(groupRef);
   flex-direction: row;
   font-family: var(--vkui--font_family_base);
   justify-content: flex-start;
+  gap: 6px;
 }
 
 .a-group-counter__count {
@@ -67,25 +56,6 @@ const counters = useGroupCounters(groupRef);
   display: flex;
   justify-content: center;
   margin-left: auto;
-  min-height: 36px;
-  min-width: 36px;
-}
-
-.a-group-counter__count-wrapper {
-  align-items: center;
-  background: var(--counter_primary_background);
-  border-radius: 12px;
-  color: var(--counter_primary_text);
-  display: flex;
-  font-size: var(--vkui--font_headline2--font_size--compact, 14px);
-  font-weight: 400;
-  height: 24px;
-  justify-content: center;
-  line-height: var(--vkui--font_headline2--line_height--compact, 20px);
-  min-width: 24px;
-
-  padding-left: 5px;
-  padding-right: 5px;
-  width: max-content;
+  font-weight: 600;
 }
 </style>
